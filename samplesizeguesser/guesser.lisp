e1cbf48327ef59f373519f982c46c6d0a15511b5
fizzoo <vixirfizz@gmail.com>
2016-09-17 17:15:54 +0200

    Valley-finding in guesser

--- samplesizeguesser/guesser.lisp
+++ samplesizeguesser/guesser.lisp
@@ -17,12 +17,35 @@ numerator/denominator form."
       denominator-guess
       (close-lcm list (1+ denominator-guess) tolerance)))
 
-(defun list-good-lcms (list)
+(defun list-of-good-lcms (list &optional (upto 10000))
+  "Creates a list of upto elements, containing the MSE of using the
+denominator corresponding to that index."
   (loop
-     with lasterr = 100000
-     for denom from 1 to 10000
-     do (cond ((< (mse-of-denom-list list denom) lasterr)
-               (format t "denominator ~a, error ~a~%" denom (mse-of-denom-list list denom))
-               (setf lasterr (mse-of-denom-list list denom))))))
+     for denom from 1 to upto
+     collecting (mse-of-denom-list list denom)))
+
+(defun find-valleys (list &key (cur-index 1))
+  "Finds valleys in values in list, returning pairs of an index and
+value of those valleys"
+  (cond ((or (not list) (not (cdr list)) (not (cddr list)))
+         nil)
+        ((and (> (car list) (cadr list))
+              (> (caddr list) (cadr list)))
+         (cons (cons cur-index (cadr list))
+               (find-valleys (cdr list)
+                             :cur-index (1+ cur-index))))
+        (t
+         (find-valleys (cdr list)
+                       :cur-index (1+ cur-index)))))
+
+(defun print-good-lcms (list)
+  (loop
+     for v in (find-valleys (list-of-good-lcms list))
+     do (format t "denominator ~a, error ~a~%" (car v) (cdr v))))
+
 
 ;;; Could make a "lowest with all digits correct" sort of thing. TODO.
+
+;; collecting (cond ((< (mse-of-denom-list list denom) lasterr)
+;;           (format t "denominator ~a, error ~a~%" denom (mse-of-denom-list list denom))
+;;           (setf lasterr (mse-of-denom-list list denom))))))
