9cb97e9266c621305c7af631d60cb5dde906599c
fizzoo <vixirfizz@gmail.com>
2018-11-07 17:42:26 +0100

    statusypp: todo

--- statusypp/statusy.cc
+++ statusypp/statusy.cc
@@ -6,6 +6,14 @@
 //namespaced either, so it is not very clean anyway.
 using namespace std;
 
+/* todo/thoughts for improvement:
+Make a nice clock somehow. maybe just figlet.
+Otherwise re-implement statusy in nicer code.
+Have different styles for printing, e.g. left-adjusted, center-each-line, color? prettifying box?
+(maybe skip) let one specify the commands and the style in a lisp/json file
+ */
+
+
 // RAII driven NCurses init and exit
 class CursesWrap {
 private:
